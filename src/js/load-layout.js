dd7d683d2d6c0ae1f1906723b0235fcbcc225efc
konarfil <filip.konarik@ysoft.com>
2023-09-22 15:31:41 +0200

    Various fixes and improvements

--- src/js/load-layout.js
+++ src/js/load-layout.js
@@ -1,2 +1,2 @@
-$(".page-footer").load("layout/footer.html");
-$("#page-header").load("layout/header.html");
+$(".page-footer").load("/layout/footer.html");
+$("#page-header").load("/layout/header.html");
