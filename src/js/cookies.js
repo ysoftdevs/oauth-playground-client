3cf5463c08a901e752007ee2c674c929eee9929f
konarfil <filip.konarik@ysoft.com>
2023-09-25 12:40:16 +0200

    PKCE implementation

---
+++ src/js/cookies.js
@@ -0,0 +1,12 @@
+function setCookie(name, value, minutes) {
+    const date = new Date();
+    date.setTime(date.getTime() + (minutes * 60 * 1000));
+    const expires = "; expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
